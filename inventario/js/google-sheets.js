3e0bc9cf20fa2e085da0b5975a416821ef69b52a
Jesus Rivero <jesus.rivero86@gmail.com>
2025-10-01 13:32:09 -0400

    Update google-sheets.js

--- inventario/js/google-sheets.js
+++ inventario/js/google-sheets.js
@@ -1,6 +1,6 @@
 class GoogleSheetsAPI {
     constructor() {
-        this.SPREADSHEET_ID = 'TU_ID_DE_HOJA_AQUI'; // Reemplazar con tu ID
+        this.SPREADSHEET_ID = '1tm1OKWzWB8K1y9i_CvBoI5xPLW7hjxDIqJ8qaowZa1c'; // Reemplazar con tu ID
         this.API_KEY = 'TU_API_KEY_AQUI'; // Opcional para solo lectura
         this.sheetName = 'INVENTARIO';
     }
