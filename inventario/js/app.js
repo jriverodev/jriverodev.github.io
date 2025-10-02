253d83b38c60ca0b4a828a87e142b3386ce64b00
Jesus Rivero <jesus.rivero86@gmail.com>
2025-10-02 14:19:16 -0400

    Update app.js

--- inventario/js/app.js
+++ inventario/js/app.js
@@ -11,7 +11,7 @@ class InventoryApp {
     this.inventoryData = [];
     this.filteredData = [];
     // URL de la Web App de Google Apps Script - REEMPLAZAR CON TU URL
-    this.webAppUrl = 'https://script.google.com/macros/s/1tm1OKWzWB8K1y9i_CvBoI5xPLW7hjxDIqJ8qaowZa1c/exec';
+    this.webAppUrl = 'https://script.google.com/macros/s/AKfycby-yuPxKCGc0dt_CaHyF4IKwCbI3-pcICgwpLYq3N4oiLcNj0EHPwkCCZKROX7H962Z/exec';
     this.init();
   }
 
