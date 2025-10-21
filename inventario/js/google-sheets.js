b18a22e67f438b594ef2757629b695bb077f0dd0
Jesus Rivero <jesus.rivero86@gmail.com>
2025-10-21 14:26:24 -0400

    Update google-sheets.js

--- inventario/js/google-sheets.js
+++ inventario/js/google-sheets.js
@@ -28,7 +28,7 @@ class GoogleSheetsAPI {
 
      */
 
-    this.scriptURL = 'https://script.google.com/macros/s/AKfycbxdhD0tU5OSveUQNGGReKMLETALlIKjljd5zkJC6y6Viw_jF6o8AFY2d-Z9GuUo7sWY/exec';
+    this.scriptURL = 'https://script.google.com/macros/s/AKfycbznoxXMaauGkBAEh-Cy1gTFscwE8gGW7z6eTr9dUr_wpUn7v2xWLGjyPKkhUotd5JBd/exec';
 
   }
 
