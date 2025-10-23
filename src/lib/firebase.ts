2155d5adfbe088b5db11d988d99c451a875bb92e
Jesus Rivero <jesus.rivero86@gmail.com>
2025-10-23 14:15:17 +0000

    Ya se puede colocar en producción?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// IMPORTANT: Move these to environment variables in a real application
 const firebaseConfig = {
-  apiKey: "AIzaSyAyp6MSHLvmtdzDRVXqMU7wkRn4tWypA2I",
-  authDomain: "jesus-rivero.firebaseapp.com",
-  projectId: "jesus-rivero",
-  storageBucket: "jesus-rivero.appspot.com",
-  messagingSenderId: "74695325616",
-  appId: "1:74695325616:web:c4c92ba15e1b50874d0875",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
