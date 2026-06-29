58545f2d6326baf2dc293486c07e404fb7f53d5c
Jesus Rivero <jesus.rivero86@gmail.com>
2026-06-29 01:14:59 -0400

    Update app.js

--- tto-gestion-flota/js/app.js
+++ tto-gestion-flota/js/app.js
@@ -5,7 +5,7 @@
 // =========================================================================
 const APP_CONFIG = {
     // REEMPLAZA ESTA URL CON EL LINK DE TU DESPLIEGUE EN GOOGLE APPS SCRIPT
-    URL_API: "https://script.google.com/macros/s/AKfycbx9Uf8cFB3wTMk_vGxBGPxGKw2TBOUWPGJ4-IOxql8dIPO4x6dzeVcfZ9cZJSMuN4fAmw/exec"
+    URL_API: "https://script.google.com/macros/s/AKfycbzjV0ARbB1kMrnYwH4sPqV6wPdcBxmw4ngJZqdSZUnn7dkmZ9S0ubmXVPfWySCcmSAGrg/exec"
 };
 
 // Registro del Service Worker para soporte de PWA (Instalación y caché local)
