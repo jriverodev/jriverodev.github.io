3e2c4a3a3e8e69a4b3d2e8435ad32650cfb90b03
Jesus Rivero <jesus.rivero86@gmail.com>
2026-07-13 18:22:38 -0400

    Fix typo in authorized users list

--- asistenciamtto/app.js
+++ asistenciamtto/app.js
@@ -11,7 +11,7 @@ createApp({
                "JEAN PIRELA": "jp775",
                "DEIBI TUDARES": "dt137",
                "VANESSA ROMERO": "vr061",
-               "EDGAR DELMORA": "ed111"
+               "EDGAR DELMORAL": "ed110"
                 
             },
             operadorActual: sessionStorage.getItem("TTOCC_OPERADOR") || null,
