9449a4fb6d7074a4f773c25c0b54d3c7e99b6e06
Jesus Rivero <jesus.rivero86@gmail.com>
2026-09-09 00:34:38 -0400

    Fix policy creation for public access on objects

--- tto-gestion-flota/migrations/enable_rls_policies.sql
+++ tto-gestion-flota/migrations/enable_rls_policies.sql
@@ -41,7 +41,7 @@ BEGIN
   IF EXISTS (SELECT 1 FROM pg_tables WHERE schemaname = 'storage' AND tablename = 'objects') THEN
     EXECUTE '
       DROP POLICY IF EXISTS "Permitir acceso publico a siagop-archivos" ON storage.objects;
-      CREATE POLICY "Permitir acceso publico a ttocc-archivos"
+      CREATE POLICY "Permitir acceso publico a siagop-archivos"
         ON storage.objects FOR ALL
         TO anon, authenticated
         USING (bucket_id = ''siagop-archivos'')
